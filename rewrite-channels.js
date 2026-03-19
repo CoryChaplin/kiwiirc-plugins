577c35323ff5053ad79ca4f7386771d1dd65822d
Cory Chaplin <cory.chaplin@laposte.net>
2026-03-19 20:52:18 +0100

    fix: remove channels from url entirely

--- rewrite-channels.js
+++ rewrite-channels.js
@@ -16,13 +16,12 @@ kiwi.plugin('url-channels-rewriter', function (kiwi, log) {
         .map((c) => c[0] !== '#' ? '#' + c : c)
         .filter((c, pos, self) => c.length >= 2 && self.indexOf(c) === pos);
 
-    if (window.history.pushState) {
-        // update url to remove channels query param
-        const url = new URL(window.location);
-        url.searchParams.delete('channel');
-        window.history.pushState({}, '', url);
-    }
-
-    // add all channels to location hash
-    window.location.hash = channel.join(',');
+    kiwi.on('irc.registered', function (event, network) {
+        if (window.history.pushState) {
+            // update url to remove channels query param
+            const url = new URL(window.location);
+            url.searchParams.delete('channel');
+            window.history.pushState({}, '', url);
+        }
+    });
 });
